b71c1ffdafb6e827fb272630a29b8159bf891f80
Will Haskell <wlhaskell@gmail.com>
2016-04-20 19:41:43 -0400

    Added band Photos

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,5 @@ class Artist < ActiveRecord::Base
 	has_many :songs
 	validates :name, presence: true
 	validates :name, uniqueness: true
+	mount_uploader :photo, PhotoUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420200256) do
+ActiveRecord::Schema.define(version: 20160420232019) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "rating"
+    t.string   "photo"
   end
 
   create_table "songs", force: :cascade do |t|
